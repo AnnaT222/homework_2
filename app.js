79793ed1fa62072740f5164346f65b327e53e453
AnnaT222 <annatazaian@gmail.com>
2023-05-22 16:22:58 +0400

    changes done

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 console.log("Anna");
-import  faker  from "@faker-js/faker";
+import  {faker}  from "@faker-js/faker";
 import  lodash  from "lodash";
-// const { faker } = require('@faker-js/faker')
-// const lodash = require('lodash');
 const users = []
 const createUsers = (count) => {
     for (let i = 0; i < count; i++) {
